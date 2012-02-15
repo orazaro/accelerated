055759beb12c530bcf817357f7a524df83fdac6f
orazaro <oraz@rambler.ru>
2012-02-16 01:19:21 +0400

    palindrom using algorithm: equal

---
+++ 06-00-palindromes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+int main()
+{
+	typedef std::string::const_iterator iter;
+	std::string word;
+	std::string::size_type maxlen = 0;
+	std::string poly;
+	while(std::cin >> word) {
+		if( equal(word.begin(), word.end(), word.rbegin()) ) {
+			if(word.size() > maxlen) {
+				maxlen = word.size();
+				poly = word;
+			}
+		}
+	}
+	std::cout << "palindrom: " << poly << std::endl;
+	return 0;
+}
