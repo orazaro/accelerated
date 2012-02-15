33a81e246cd646981dcedadcb5be60ad899751a2
orazaro <oraz@rambler.ru>
2012-02-16 01:46:38 +0400

    rbegin() as reverse_iterator

---
+++ 06-00-rbackup.cpp
@@ -0,0 +1,15 @@
+// use of rbegin() as reverse iterator
+#include <iostream>
+#include <string>
+
+int main()
+{
+	typedef std::string::const_reverse_iterator iter;
+	std::string word;
+	while(std::cin >> word) {
+		for(iter r = word.rbegin(); r != word.rend(); r++)
+			std::cout << *r;
+		std::cout << std::endl;
+	}
+	return 0;
+}
