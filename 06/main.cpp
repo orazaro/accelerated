8fcc3a78cbb76a6ed9e63c3bcfab73453785cfa3
orazaro <oraz@rambler.ru>
2012-02-17 16:45:03 +0400

    did_all_hw
    read all students, separating them based on whether all homework was done

--- 06/main.cpp
+++ 06/main.cpp
@@ -10,19 +10,37 @@
 #include "grade.h"
 #include "Student_info.h"
 
+bool did_all_hw(const Student_info& s)
+{
+    return (
+        find(s.homework.begin(), s.homework.end(), 0)
+        == s.homework.end()
+        );
+}
+
 int main()
 {
-    StudentList students;
+    using namespace std;
+    vector<Student_info> did, didnt;
+    Student_info student;
 
-    read(std::cin, students);
-
-    StudentList fail = extract_fails(students);
-
-    std::cout << "pass:" << std::endl;
-    out_students(students, std::cout);
-    std::cout << "fail:" << std::endl;
-    out_students(fail, std::cout);
+    // read all students, separating them based on whether all homework was done
+    while(read(cin, student)) {
+        if(did_all_hw(student))
+            did.push_back(student);
+        else
+            didnt.push_back(student);
+    }
     
+    if(did.empty()) {
+        cout << "No student did all the homework!" << endl;
+        return 1;
+    }
+    if(didnt.empty()) {
+        cout << "Every student did all the homework!" << endl;
+        return 1;
+    }
+
     return 0;
 }
 
