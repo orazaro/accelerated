874e51eff1615d54a981a91c2958d369618a08cd
orazaro <oraz@rambler.ru>
2012-02-20 13:49:24 +0400

    -depth => -maxdepth

--- github_pullall.sh
+++ github_pullall.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-for d in `find . -type d -depth 1`;do echo $d" ===>";cd $d && git pull ; cd ..;done
+for d in `find . -maxdepth 1 -type d`;do echo $d" ===>";cd $d && git pull ; cd ..;done
