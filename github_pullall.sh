798a2ba6c3c167464deb63817ee3511b261a2270
orazaro <oraz@rambler.ru>
2012-02-21 14:06:52 +0400

    fix problems with find: use ls

--- github_pullall.sh
+++ github_pullall.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-for d in `find . -maxdepth 1 -type d`;do echo $d" ===>";cd $d && git pull ; cd ..;done
+for d in `ls`;do if [ -d $d ]; then echo "===> $d";cd $d && git pull ; cd ..; fi;done
