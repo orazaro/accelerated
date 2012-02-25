4f69599ba1d814b23fddae97371ac18840e4bdb3
orazaro <oraz@rambler.ru>
2012-02-25 22:00:40 +0400

    07-09: nrand(n) in case n > RAND_MAX

---
+++ 07-09-nrand.cpp
@@ -0,0 +1,50 @@
+/* 07-09: nrand if n > RAND_MAX
+to test we imitate that this case with trandom() */
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <stdexcept>
+#include <vector>
+
+const int TRAND_MAX = 1000;
+int trandom()
+{
+    return random() % TRAND_MAX;
+}
+
+// return a random integer in the range [0, n)
+int nrand(int n)
+{
+    if(n <= 0)
+        throw std::domain_error("Argument to nrand is out of range");
+    int result = 0;
+    if(n > TRAND_MAX) {
+        int parts = n / TRAND_MAX;
+        for(int i = 0; i < parts; ++i)
+            result += trandom();
+    }
+    int m = n % TRAND_MAX;
+    const int bucket_size = TRAND_MAX / m;
+    int r;
+    do r = trandom() / bucket_size;
+    while(r >= m);
+    return result + r;
+}
+
+int main()
+{
+    using namespace std;
+    const int n_gist = 10;
+    vector<int> gist(n_gist);
+    srandom(time(NULL));
+    
+    for(int i = 0; i < 10000000; ++i)
+    {
+        int r = nrand(99999);
+        gist[r % n_gist]++;
+    } 
+    for(int i = 0; i < n_gist; ++i) {
+        std::cout << i << "\t" << gist[i] << std::endl;
+    }
+    return 0;
+}
